ca599e6a94e7807cd0d07af2356d96af02159c73
mallynnk <mallory.korpics@gmail.com>
2020-06-28 10:12:27 -0500

    added saving score function

--- assets/js/script1.js
+++ assets/js/script1.js
@@ -1,11 +1,17 @@
 const timeLeftDisplay = document.querySelector("#time-left")
-const scoreDisplay = document.querySelector("#score")
+// const scoreDisplay = document.querySelector("#score")
 const startBtn = document.querySelector("#start-btn")
+const initialBtn = document.querySelector("#initials-btn")
+const initialInput = document.querySelector("#initial-input")
+const scoreDisplay = document.getElementById("high-scores")
 const startButton = document.getElementById("start-btn")
 const nextButton= document.getElementById("next-btn")
 const questionContainerEl = document.getElementById("question-container")
 const questionEl = document.getElementById("question")
 const answerButtonsEl = document.getElementById("answer-buttons")
+const initialEl = document.getElementById("initials")
+const highScores = []
+
 let timeLeft = 30;
 let score = 0;
 let shuffledQuestions, currentQuestionIndex 
@@ -14,12 +20,14 @@ let shuffledQuestions, currentQuestionIndex
 // function to begin quiz
 function startQuiz() {
     startButton.classList.add("hide")
+    scoreDisplay.classList.add("hide")
+    
     questionContainerEl.classList.remove("hide")
     shuffledQuestions = questions.sort(() => Math.random() - .5)
     currentQuestionIndex = 0
     questionContainerEl.classList.remove("hide") 
     setNextQuestion();
-    keepScore()
+    // keepScore()
 }
 
 
@@ -28,39 +36,27 @@ function startQuiz() {
     setInterval(function(){
         if(timeLeft <= 0) {
             clearInterval(timeLeft = 0);
+            enterInitials();
            // window.alert("Your time is up!")
-            //startQuiz()
-        }
-        //console.log(timeLeft);
+        } 
         timeLeftDisplay.innerHTML = timeLeft
         timeLeft -= 1
     }, 1000)
 }
 
 
- //score function   
- function keepScore() {
-    setInterval(function(){
-        if(answer.correct) {
-            score = score +1
-        } else {
-            timeLeft = timeLeft -2
-        }
-        console.log(score);
-        scoreDisplay.innerHTML = score
-    })
-}
-
-
-function keepScore() {
-    if(question = true) {
-        score = score++
-        console.log(score)
-    } else {
-    timeLeft = timeLeft -2 
-    }
-}
-
+//  //score function   
+//  function keepScore() {
+//     setInterval(function(){
+//         if(answer.correct) {
+//             score = score +1
+//         } else {
+//             timeLeft = timeLeft -2
+//         }
+//         console.log(score);
+//         scoreDisplay.innerHTML = score
+//     })
+// }
 
 // function to begin next Question
 function setNextQuestion() {
@@ -69,66 +65,98 @@ function setNextQuestion() {
     }
 
     //function to make question appear
-
-    function showQuestion(question) {
-        questionEl.innerText = question.question
-        question.answers.forEach(answer => {
-            const button = document.createElement("button")
-            button.innerText= answer.text
-            button.classList.add("btn")
-            if (answer.correct) {
-                button.dataset.correct = answer.correct
-            }
-            button.addEventListener("click", selectAnswer)
-            answerButtonsEl.appendChild(button) 
-        })
-    } 
-
-    //change the visible answers
-    function resetState() {
-        nextButton.classList.add("hide")
-        while (answerButtonsEl.firstChild) {
-            answerButtonsEl.removeChild
-            (answerButtonsEl.firstChild)
+function showQuestion(question) {
+    questionEl.innerText = question.question
+    question.answers.forEach(answer => {
+        const button = document.createElement("button")
+        button.innerText= answer.text
+        button.classList.add("btn")
+        if (answer.correct) {
+            button.dataset.correct = answer.correct
         }
+        button.addEventListener("click", selectAnswer)
+        answerButtonsEl.appendChild(button) 
+    })
+} 
+//change the visible answers
+function resetState() {
+    nextButton.classList.add("hide")
+    while (answerButtonsEl.firstChild) {
+        answerButtonsEl.removeChild
+        (answerButtonsEl.firstChild)
+    }
+}
+
+//shuffle through questions array
+function selectAnswer(e) {
+    const selectedButton = e.target
+    const correct = selectedButton.dataset.correct
+    setStatusClass(document.body, correct)
+    Array.from(answerButtonsEl.children).forEach(button => { 
+        setStatusClass(button, button.dataset.correct)
+    })
+    if (shuffledQuestions.length > currentQuestionIndex + 1) {
+        currentQuestionIndex++
+        setNextQuestion()
+    } else {
+        enterInitials()
+    
     }
     
-    //shuffle through questions array
-    function selectAnswer(e) {
-        const selectedButton = e.target
-        const correct = selectedButton.dataset.correct
-        setStatusClass(document.body, correct)
-        Array.from(answerButtonsEl.children).forEach(button => { 
-            setStatusClass(button, button.dataset.correct)
-        })
-        if (shuffledQuestions.length > currentQuestionIndex + 1) {
-            nextButton.classList.remove("hide")
-        } else {
-            startButton.innerText = "restart"
-            startButton.classList.remove("hide") 
-        }
-        
-    }
-    
-    //correct and wrong functions
-    function setStatusClass(element, correct) {
-        clearStatusClass(element)
-        if (correct) {
-            element.classList.add("correct")
-        } else {
-            element.classList.add("wrong")
-        }
-        console.log(element);
-    }
-    
-    function clearStatusClass(element) {
-        element.classList.remove("correct")
-        element.classList.remove("wrong")
-    }
+}
 
+//correct and wrong functions
+function setStatusClass(element, correct) {
+    clearStatusClass(element)
+    if (correct) {
+        element.classList.add("correct")
+    } else {
+        element.classList.add("wrong")
+    }
+    console.log(element);
+}
+
+function clearStatusClass(element) {
+    element.classList.remove("correct")
+    element.classList.remove("wrong")
+}
+
+//intials function
+function enterInitials() {
+    initialEl.classList.remove("hide")
+    questionContainerEl.classList.add("hide")
+}
+
+//save score
+function saveScore() {
+//    var saveScore = {initials: initialInput.value, 
+//      score: "score" 
+//     }
+    // highScores.push(saveScore)
+        var scoreDiv = document.createElement("div")
+        scoreDiv.innerHTML = "<h2>" + initialInput.value + "</h2><div>" + "score placeholder" + "</div"
+        document.getElementById("scoreContainer").append(scoreDiv)
+    displayScores()
+}
+
+//ensure initials are entered before saving
+function handleSubmitButtonState() {
+   if (initialInput.value) {
+       initialBtn.removeAttribute("disabled")
+   } else {
+      initialBtn.setAttribute("disabled", true)
+   }
+
+}
+//display scores
+function displayScores() {
+    initialEl.classList.add("hide")
+    scoreDisplay.classList.remove("hide")
+    startButton.innerText = "restart"
+    startButton.classList.remove("hide") 
+}
 
 //questions array
-
 var questions = [
     {
         question: "What is DOM?",
@@ -165,4 +193,6 @@ nextButton.addEventListener("click", () => {
     currentQuestionIndex++
     setNextQuestion()})
 startBtn.addEventListener("click", countDown)
+initialBtn.addEventListener("click", saveScore)
+initialInput.addEventListener("input", handleSubmitButtonState)
 })
